96ba05109531d9a0e85cb68816e3ceafc6fe61ba
Ryan <rkeen3@gmail.com>
2026-03-07 10:42:03 +0000

    Fix place pages to render dynamically

--- app/place/[slug]/page.js
+++ app/place/[slug]/page.js
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+
 import { notFound } from "next/navigation";
 import { getAllPlaces, getPlaceBySlug } from "@/lib/places";
 
